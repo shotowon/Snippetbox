22852c3640c715b13dc3d1e3b338755468fb3d21
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-02 19:21:38 +0500

    homepage handler

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, from Snippetbox"))
 }
