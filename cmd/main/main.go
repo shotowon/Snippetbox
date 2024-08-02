1a452b9a0c1c4cdcdf398138c0d8b39565e31157
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-02 19:25:54 +0500

    web server functionality

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	addr := ":4000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Printf("Starting server on %s", addr)
+	server := &http.Server{
+		Handler: mux,
+		Addr:    addr,
+	}
+
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
