1677553712360118bbbc41209392d54519a1a380
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 01:09:46 +0500

    add environment variable into config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import "flag"
 
 type config struct {
+	Env             string
 	Addr            string
 	StaticDirectory string
 }
@@ -10,6 +11,7 @@ type config struct {
 func Load() *config {
 	var cfg config
 
+	flag.StringVar(&cfg.Env, "SNIPPETBOX_ENV", "LOCAL", "Environment in which Snippetbox is running")
 	flag.StringVar(&cfg.Addr, "SNIPPETBOX_ADDR", ":4000", "Address of Snippetbox HTTP Server")
 	flag.StringVar(&cfg.StaticDirectory, "SNIPPETBOX_STATIC", "./ui/static", "Path to static assets")
 
