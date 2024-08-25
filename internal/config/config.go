201d91e36647604e2364ed56a0e030ad432058e3
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 03:32:24 +0500

    add logfile to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,14 +6,16 @@ type config struct {
 	Env             string
 	Addr            string
 	StaticDirectory string
+	LogFile         string
 }
 
 func Load() *config {
 	var cfg config
 
-	flag.StringVar(&cfg.Env, "SNIPPETBOX_ENV", "LOCAL", "Environment in which Snippetbox is running")
+	flag.StringVar(&cfg.Env, "SNIPPETBOX_ENV", EnvLocal, "Environment in which Snippetbox is running")
 	flag.StringVar(&cfg.Addr, "SNIPPETBOX_ADDR", ":4000", "Address of Snippetbox HTTP Server")
 	flag.StringVar(&cfg.StaticDirectory, "SNIPPETBOX_STATIC", "./ui/static", "Path to static assets")
+	flag.StringVar(&cfg.LogFile, "SNIPPETBOX_LOGFILE", "snippetbox.log", "File to write logs")
 
 	flag.Parse()
 	return &cfg
