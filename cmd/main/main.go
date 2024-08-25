b1204f357b85bf8f580d065d5474298ab7bb9c2f
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 01:02:15 +0500

    config loading and usage

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,22 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"snippetbox/internal/config"
 )
 
 func main() {
-	addr := ":4000"
+	cfg := config.Load()
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDirectory))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", addr)
+	log.Printf("Starting server on %s", cfg.Addr)
 	server := &http.Server{
 		Handler: mux,
-		Addr:    addr,
+		Addr:    cfg.Addr,
 	}
 
 	if err := server.ListenAndServe(); err != nil {
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,17 @@
 package config
 
+import "flag"
+
 type config struct {
 	Addr            string
 	StaticDirectory string
+}
+
+func Load() *config {
+	var cfg config
+
+	flag.StringVar(&cfg.Addr, "SNIPPETBOX_ADDR", ":4000", "Address of Snippetbox HTTP Server")
+	flag.StringVar(&cfg.StaticDirectory, "SNIPPETBOX_STATIC", "./ui/static", "Path to static assets")
+
+	return &cfg
 }
