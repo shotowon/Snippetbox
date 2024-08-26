07669d998359f186ad417c1cdb2feb372334b413
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 05:53:02 +0500

    update main

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"context"
 	"log/slog"
-	"net/http"
-	"os"
+	"snippetbox/internal/app"
 	"snippetbox/internal/config"
 	"snippetbox/internal/logger"
 )
@@ -14,21 +14,6 @@ func main() {
 	_ = logger.SetAndReturn(cfg.Env, cfg.LogFile)
 	slog.Info("Configuration loaded")
 
-	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(cfg.StaticDirectory))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-
-	slog.Info("Starting server", slog.String("address", cfg.Addr))
-	server := &http.Server{
-		Handler: mux,
-		Addr:    cfg.Addr,
-	}
-
-	if err := server.ListenAndServe(); err != nil {
-		slog.Error(err.Error())
-		os.Exit(1)
-	}
+	app := app.New(cfg)
+	app.Run(context.Background())
 }
