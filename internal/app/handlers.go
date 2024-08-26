80d2a059446d9b5cb52ce8376adb1d5e8febc20f
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 05:38:26 +0500

    move handlers to application

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -1,4 +1,4 @@
-package main
+package app
 
 import (
 	"fmt"
@@ -8,7 +8,7 @@ import (
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (a *application) home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
@@ -28,7 +28,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (a *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
@@ -38,7 +38,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Create snippet..."))
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (a *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
