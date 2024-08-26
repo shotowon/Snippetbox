8b3c2fe6ce17136ba11408973d62a21936d5f777
ppaprikaa <sanzhar.maratov@proton.me>
2024-08-26 05:45:37 +0500

    router setup

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,3 +9,14 @@ type application struct {
 	cfg    *config.Config
 	server *http.Server
 }
+
+func (a *application) router(cfg *config.Config) http.Handler {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir(cfg.StaticDirectory))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("/", a.home)
+	mux.HandleFunc("/snippet/view", a.snippetView)
+	mux.HandleFunc("/snippet/create", a.snippetCreate)
+
+	return mux
+}
