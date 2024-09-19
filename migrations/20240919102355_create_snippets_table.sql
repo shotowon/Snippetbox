0d226f7dc6da6362d1aaf3a44a920bbebddce13d
ppaprikaa <sanzhar.maratov@proton.me>
2024-09-19 17:05:32 +0500

    create snippets table sql migration

---
+++ migrations/20240919102355_create_snippets_table.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE snippets (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	content TEXT NOT NULL,
+	created TIMESTAMP DEFAULT NOW(),
+	expires TIMESTAMP NOT NULL
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS snippets;
+-- +goose StatementEnd
