e9eda351f8fb7621449942fdb841a131086abd39
ppaprikaa <sanzhar.maratov@proton.me>
2024-09-29 23:02:36 +0500

    add DSN field to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,7 @@ type Config struct {
 	Addr            string
 	StaticDirectory string
 	LogFile         string
+	MainDSN         string
 }
 
 func Load() *Config {
@@ -16,6 +17,7 @@ func Load() *Config {
 	flag.StringVar(&cfg.Addr, "SNIPPETBOX_ADDR", ":4000", "Address of Snippetbox HTTP Server")
 	flag.StringVar(&cfg.StaticDirectory, "SNIPPETBOX_STATIC", "./ui/static", "Path to static assets")
 	flag.StringVar(&cfg.LogFile, "SNIPPETBOX_LOGFILE", "snippetbox.log", "File to write logs")
+	flag.StringVar(&cfg.MainDSN, "SNIPPETBOX_MAIN_DSN", "postgres://snippetbox:snippetbox@localhost:5050/snippetbox", "link to main DSN of snippetbox")
 
 	flag.Parse()
 	return &cfg
